8eb9b8332252a15ed8d067581f3af0942e272f03
mehmet <mehmet@mehmet-pardus.(none)>
2010-02-17 15:37:49 +0200

    zenit tammam gibi...

--- src/functions/zenith.py
+++ src/functions/zenith.py
@@ -12,6 +12,8 @@ class Zenith:
         self.v_angles_2 = []
         self.counter = 0
         self.horizons = [] 
+        self.zeniths_points_bn = []
+        self.zeniths_points_dn = []
         self.line_seperator(self.logfile)
 
     def line_seperator(self, logfile):
@@ -38,35 +40,48 @@ class Zenith:
     def cal_zenith(self, lines):
         i=0
         while i < len(lines):
-            zenit = float(self.v_angles_1[i]) + ((400 - (float(self.v_angles_1[i])+float(self.v_angles_2[i] )))/2)
-            self.zeniths.append(zenit)
-            i+=1
             if self.v_angles_1[i] == "":
-                break
+                i+=1
+            else:
+                zenit = float(self.v_angles_1[i]) + ((400 - (float(self.v_angles_1[i])+float(self.v_angles_2[i] )))/2)
+                self.zeniths.append(zenit)
+                self.zeniths_points_bn.append(self.bakilan_nokta[i])
+                self.zeniths_points_dn.append(self.durulan_nokta[i])
+                i+=1
 
     def write(self, lines, zeniths):
+        #print self.zeniths_points_dn, self.zeniths_points_bn, self.zeniths
         file = open('../../tmp/zeniths', 'w')
         i=0
-        line_count = len(lines)/2 #
-        self.serial_count("101", lines)
+        line_count = len(zeniths)
         while i < line_count:
-            if self.zeniths[i]:
-                sta_zenit = self.zeniths[i]
-            else:
-                sta_zenit = "-"
-            line = 'DN: %s BN: %s Zenit: %s \n' % (self.durulan_nokta[i], self.bakilan_nokta[i], sta_zenit)
+            #print self.serial_count(self.zeniths_points_dn[i], self.zeniths_points_bn[i], self.zeniths)
+            zenit = self.zenit(self.zeniths_points_dn[i], self.zeniths_points_bn[i], self.zeniths)/self.serial_count(self.zeniths_points_dn[i], self.zeniths_points_bn[i], self.zeniths)
+            line = 'DN: %s BN: %s Zenit: %s \n' % (self.zeniths_points_dn[i], self.zeniths_points_bn[i], zenit)
+            print line
             file.write(line)
             i+=1
         file.close()
 
 
-    def serial_count(self, durulan_nokta, lines):
+    def serial_count(self, durulan_nokta, bakilan_nokta, zeniths):
         counter = 0
-        while counter < len(lines):
-            word = lines[counter].split("/")
+        serial = 0
+        while counter < len(zeniths):
+            if self.zeniths_points_bn[counter] == bakilan_nokta and self.zeniths_points_dn[counter] == durulan_nokta:
+                serial+=1
             counter+=1
-            if durulan_nokta == word[0]:
-                print word[1], self.bakilan_nokta.count(word[1])
+        return serial
+
+    def zenit(self, durulan_nokta, bakilan_nokta, zeniths):
+        counter = 0
+        zenit = 0
+        while counter < len(zeniths):
+            if self.zeniths_points_bn[counter] == bakilan_nokta and self.zeniths_points_dn[counter] == durulan_nokta:
+                #print durulan_nokta, bakilan_nokta, zenit
+                zenit+= zeniths[counter]
+            counter+=1
+        return zenit
 
 if __name__ == "__main__":
     instant = Zenith()
