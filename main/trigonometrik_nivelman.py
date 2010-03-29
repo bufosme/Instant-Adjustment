d3e6c912eeae8c59ec3918412701c3220e55e6e0
mehmet <mehmet@mehmet-pardus.(none)>
2010-03-29 17:55:35 +0300

    formulde hata düzeltildi

--- main/trigonometrik_nivelman.py
+++ main/trigonometrik_nivelman.py
@@ -19,6 +19,8 @@ class trigonometrik_niv:
         self.h = []
         self.delta_h = []
         self.fark = []
+        self.a = []
+        self.b = []
         self.counter = 0
         self.seperator_trigonometrik(self.trigonometrik_log)
         self.seperator_kot(self.kot_log)
@@ -35,13 +37,14 @@ class trigonometrik_niv:
         while counter < len(trigonometrik_lines):
             word = trigonometrik_lines[counter].split("/")
             if word[0] !="#":
-                self.DN.append(float(word[0]))
-                self.BN.append(float(word[1]))
+                self.DN.append(word[0])
+                self.BN.append(word[1])
                 self.a_yuk.append(float(word[2]))
                 self.r_yuk.append(float(word[3]))
                 self.slope_dist.append(float(word[4]))
                 self.zenith.append(float(word[5]))
             counter+=1
+        print self.zenith
 
 
 
@@ -56,35 +59,56 @@ class trigonometrik_niv:
         while counter < len(kot_lines):
             word = kot_lines[counter].split("/")
             if word[0] !="#":
-                self.NN.append(float(word[0]))
+                self.NN.append(word[0])
                 self.kot.append(float(word[1]))
             counter+=1
 
-    def fark_kontrol (self, slope_dist, zenith, a_yuk, r_yuk):
+    def * (self, slope_dist, zenith, a_yuk, r_yuk):
+
         i=0
         while i< len(self.slope_dist):
             hor_dist =(self.slope_dist[i])*sin(self.zenith[i]*pi/200)
             self.hor_dist.append(hor_dist)
             i+=1
-        print "self.hor_dist:", self.hor_dist
+        print self.hor_dist
 
         i=0
         while i < len (self.slope_dist):
-            h = self.slope_dist[i]*cos(self.zenith[i]*pi/200)
+            h =( self.slope_dist[i])*cos(self.zenith[i]*pi/200)
             self.h.append(h)
             i+=1
-        print "h:", self.h
+        print self.h
 
         i=0
         while i < len (self.slope_dist):
-            delta_h = self.h[i]+self.a_yuk[i]-self.r_yuk[i]+(0.068*(self.slope_dist[i]/1000))
+            delta_h =( self.h[i])+(self.a_yuk[i])-(self.r_yuk[i])+(0.068*(self.slope_dist[i]/1000)**2)
             self.delta_h.append(delta_h)
             i+=1
-        print "delta_h:", self.delta_h
+        print  self.delta_h
+
 
         i=0
+        while i < len(self.delta_h):
+            donus = self.delta_h[i]
+            self.b.append(donus)
+            i=i+2
+        print self.b
         
+        i=1
+        while i < len(self.delta_h):
+            gidis = self.delta_h[i]
+            self.a.append(gidis)
+            i=i+2
+        print self.a
 
+
+
+        i=0
+        while i < len(self.a):
+            delta_fark= self.a[i]+self.b[i]
+            self.fark.append(delta_fark)
+            i+=1
+        print self.fark
 
 
 
